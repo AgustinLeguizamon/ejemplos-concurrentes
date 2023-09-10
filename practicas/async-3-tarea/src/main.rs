2a6f4c58bc945c1d3c12af26d2ebe41df058da68
AgustinLeguizamon <agustinmleguizamon@gmail.com>
2023-09-10 19:20:23 -0300

    se intenta agregar request en paralelo y se mueven a static ref algunas variables

--- practicas/async-3-tarea/src/main.rs
+++ practicas/async-3-tarea/src/main.rs
@@ -1,18 +1,98 @@
 use async_std::task;
+use serde::Deserialize;
+use futures::future::join_all;
+use lazy_static::lazy_static;
+
+pub type ObservationResponseVec = Vec<ObservationResponse>;
+
+#[derive(Deserialize, Debug)]
+pub struct ObservationResponse {
+    #[serde(rename = "speciesCode")]
+    species_code: String,
+
+    #[serde(rename = "comName")]
+    com_name: String,
+
+    #[serde(rename = "sciName")]
+    sci_name: String,
+
+    #[serde(rename = "locId")]
+    loc_id: String,
+
+    #[serde(rename = "locName")]
+    loc_name: String,
+
+    #[serde(rename = "obsDt")]
+    obs_dt: String,
+
+    #[serde(rename = "howMany")]
+    how_many: i64,
+
+    #[serde(rename = "lat")]
+    lat: f64,
+
+    #[serde(rename = "lng")]
+    lng: f64,
+
+    #[serde(rename = "obsValid")]
+    obs_valid: bool,
+
+    #[serde(rename = "obsReviewed")]
+    obs_reviewed: bool,
+
+    #[serde(rename = "locationPrivate")]
+    location_private: bool,
+
+    #[serde(rename = "subId")]
+    sub_id: String,
+}
+
+lazy_static! {
+    static ref API_KEY: &'static str = "ekgg6ikp80gb" ;
+    static ref REGION_CODE: &'static str = "KZ" ;
+}
 
 fn main() {
+
     println!("Hello, world!");
-    let body = task::block_on(async_main());
-
-    println!("body = {:?}", body);
+    println!("async_main = {:?}", task::block_on(async_main()));
 }
 
-async fn async_main() -> Result<String, reqwest::Error> {
+async fn async_main() -> Result<ObservationResponseVec, reqwest::Error> {
     let client = reqwest::Client::new();
-    let api_key = String::from("ekgg6ikp80gb");
-    let region_code = String::from("KZ");
-    let url = format!("https://api.ebird.org/v2/data/obs/{}/recent", region_code);
+    let url = format!("https://api.ebird.org/v2/data/obs/{}/recent", *REGION_CODE);
 
-    let body = client.get(url).header("x-ebirdapitoken", api_key).send().await?.text().await?;
-    Ok(body)
+    let observations = client
+        .get(url)
+        .query(&[("maxResults", "20")])
+        .header("x-ebirdapitoken", *API_KEY)
+        .send()
+        .await?
+        .json::<ObservationResponseVec>()
+        .await?;
+
+    println!("observations = {:?}", observations);
+    let species_codes: Vec<String> = observations.iter().map(|o| o.species_code.clone()).collect();
+    println!("species_codes = {:?}", species_codes);
+
+    //let species_observations_futures = find_observations_by_species_codes(species_codes);
+
+    Ok(observations)
 }
+/*
+async fn find_observations_by_species_codes(species_codes: Vec<String>) -> Result<Vec<ObservationResponseVec>, reqwest::Error> {
+    let observations_futures = species_codes.into_iter().map(|code| find_observations_by_species_code(code));
+    return Ok(join_all(observations_futures).await?);
+}
+
+async fn find_observations_by_species_code(code: String) -> Result<ObservationResponseVec, reqwest::Error> {
+    let client = Client::new();
+    return Ok(client
+        .get(format!("https://api.ebird.org/v2/data/obs/KZ/recent/{}", code))
+        .header("x-ebirdapitoken", &API_KEY)
+        .send()
+        .await?
+        .json::<ObservationResponseVec>()
+        .await?);
+}
+*/
