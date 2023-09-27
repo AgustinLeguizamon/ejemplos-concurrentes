5f6b2f038757fbf60c26896f4fa0e8f56801f923
AgustinLeguizamon <agustinmleguizamon@gmail.com>
2023-09-27 16:00:08 -0300

    se agrega loop infinito y se propone solucion de deadlock usando un array de estado de filosofos en un rwlock

--- practicas/sincronizacion-5-tarea/src/main.rs
+++ practicas/sincronizacion-5-tarea/src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 use std::thread;
 use std::thread::JoinHandle;
 use std::time::Duration;
@@ -7,15 +7,28 @@ use std_semaphore::Semaphore;
 
 const N: usize = 5;
 
+#[derive(PartialEq)]
+enum ESTADO {
+    PENSANDO,
+    HAMBRIENTO,
+    COMIENDO,
+}
+
 fn main() {
     let palitos:Arc<Vec<Semaphore>> = Arc::new((0 .. N)
         .map(|_| Semaphore::new(1))
         .collect());
 
+    // Modelo estado de los filosofos como un array con un Rwlock
+    // por ahora solo uso un bool para indicar si esta comiendo o no
+    let estado_filosofos = Arc::new(RwLock::new(vec![ESTADO::PENSANDO, ESTADO::PENSANDO, ESTADO::PENSANDO, ESTADO::PENSANDO, ESTADO::PENSANDO]));
+
+
     let filosofos:Vec<JoinHandle<()>> = (0 .. N)
         .map(|id| {
             let palitos_local = palitos.clone();
-            thread::spawn(move || filosofo(id, palitos_local))
+            let estado_filosofos_local = estado_filosofos.clone();
+            thread::spawn(move || filosofo(id, palitos_local, estado_filosofos_local))
         })
         .collect();
 
@@ -24,19 +37,43 @@ fn main() {
     }
 }
 
-fn filosofo(id: usize, palitos_local: Arc<Vec<Semaphore>>) {
+fn filosofo(id: usize, palitos_local: Arc<Vec<Semaphore>>, estado_filosofos_local: Arc<RwLock<Vec<ESTADO>>>) {
+    loop {
+        thread::sleep(Duration::from_millis(thread_rng().gen_range(500, 1500)));
+        println!("filosofo {} pensando", id);
+        let der = (id + 1) % 5;
+        let izq = if id == 0 { N - 1 } else { id - 1 };
+        // si resulta que mis vecinos estan esperando palitos entonces no intento tomar ninguno
+        if let Ok(mut estados_filosofos_guard) = estado_filosofos_local.write() {
+            if (*estados_filosofos_guard)[der] == ESTADO::HAMBRIENTO && (*estados_filosofos_guard)[izq] == ESTADO::HAMBRIENTO {
+                continue
+            }
+        }
 
-    println!("filosofo {} pensando", id);
+        if let Ok(mut estados_filosofos_guard) = estado_filosofos_local.write() {
+            (*estados_filosofos_guard)[id] = ESTADO::HAMBRIENTO
+        }
 
-    println!("filosofo {} esperando palito izq", id);
-    let palito_izq_guard = &palitos_local[id].access();
+        println!("filosofo {} esperando palito izq", id);
+        let palito_izq_guard = &palitos_local[id].access();
 
-    println!("filosofo {} esperando palito der", id);
-    let palito_der_guard = &palitos_local[(id + 1) % N].access();
+        println!("filosofo {} esperando palito der", id);
+        let palito_der_guard = &palitos_local[(id + 1) % N].access();
 
-    println!("filosofo {} comiendo", id);
-    thread::sleep(Duration::from_millis(thread_rng().gen_range(500, 1500)));
+        if let Ok(mut estados_filosofos_guard) = estado_filosofos_local.write() {
+            (*estados_filosofos_guard)[id] = ESTADO::COMIENDO
+        }
 
-    println!("filosofo {} termino de comer", id);
+        println!("filosofo {} comiendo", id);
+
+        thread::sleep(Duration::from_millis(thread_rng().gen_range(500, 1500)));
+
+        if let Ok(mut estados_filosofos_guard) = estado_filosofos_local.write() {
+            (*estados_filosofos_guard)[id] = ESTADO::PENSANDO
+        }
+
+        println!("filosofo {} termino de comer", id);
+    }
+
 }
 
